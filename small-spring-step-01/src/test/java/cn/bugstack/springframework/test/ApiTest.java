f4a9bb0641c0dbba527913a1f8ba655b77a0321a
小傅哥 <184172133@qq.com>
2021-07-25 11:04:01 +0800

    第 02 章：小试牛刀，实现一个简单的Bean容器

---
+++ small-spring-step-01/src/test/java/cn/bugstack/springframework/test/ApiTest.java
@@ -0,0 +1,29 @@
+package cn.bugstack.springframework.test;
+
+import cn.bugstack.springframework.BeanDefinition;
+import cn.bugstack.springframework.BeanFactory;
+import cn.bugstack.springframework.test.bean.UserService;
+import org.junit.Test;
+
+/**
+ * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ * 公众号：bugstack虫洞栈
+ * Create by 小傅哥(fustack)
+ */
+public class ApiTest {
+
+    @Test
+    public void test_BeanFactory(){
+        // 1.初始化 BeanFactory
+        BeanFactory beanFactory = new BeanFactory();
+
+        // 2.注入bean
+        BeanDefinition beanDefinition = new BeanDefinition(new UserService());
+        beanFactory.registerBeanDefinition("userService", beanDefinition);
+
+        // 3.获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
+        userService.queryUserInfo();
+    }
+
+}
