188a47bf9e763b99b1ea1fdf472b7c9302fd3bd7
小傅哥 <fuzhengwei@users.noreply.github.com>
2021-08-02 20:13:12 +0800

    Update ApiTest.java
    第 03 章：初显身手，运用设计模式，实现 Bean 的定义、注册、获取

--- small-spring-step-02/src/test/java/cn/bugstack/springframework/test/ApiTest.java
+++ small-spring-step-02/src/test/java/cn/bugstack/springframework/test/ApiTest.java
@@ -15,18 +15,18 @@ public class ApiTest {
 
     @Test
     public void test_BeanFactory(){
-        // 1.初始化 BeanFactory
+        // 1. 初始化 BeanFactory
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 
-        // 2.注册 bean
+        // 2. 注册 bean
         BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
         beanFactory.registerBeanDefinition("userService", beanDefinition);
 
-        // 3.第一次获取 bean
+        // 3. 第一次获取 bean
         UserService userService = (UserService) beanFactory.getBean("userService");
         userService.queryUserInfo();
 
-        // 4.第二次获取 bean from Singleton
+        // 4. 第二次获取 bean from Singleton
         UserService userService_singleton = (UserService) beanFactory.getBean("userService");
         userService_singleton.queryUserInfo();
     }
