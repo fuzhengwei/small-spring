1ac35a8f1b215dd74c7f659575fc1fa5bf030fd1
小傅哥 <184172133@qq.com>
2022-11-21 16:21:40 +0800

    第 13 章：行云流水，把AOP动态代理，融入到Bean的生命周期

--- small-spring-step-12/src/main/java/cn/bugstack/springframework/aop/aspectj/AspectJExpressionPointcutAdvisor.java
+++ small-spring-step-12/src/main/java/cn/bugstack/springframework/aop/aspectj/AspectJExpressionPointcutAdvisor.java
@@ -7,11 +7,11 @@ import org.aopalliance.aop.Advice;
 /**
  * Spring AOP Advisor that can be used for any AspectJ pointcut expression.
  *
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
  *
- * 来自于对开源项目的学习；
+ *
+ *
+ *
+ *
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public class AspectJExpressionPointcutAdvisor implements PointcutAdvisor {
