bd9e7e79068f37d2240ca47abde0fdceccbdd360
小傅哥 <184172133@qq.com>
2022-11-21 15:12:45 +0800

    第 16 章：战无不胜，给代理对象的属性设置值

--- small-spring-step-15/src/main/java/cn/bugstack/springframework/beans/factory/config/BeanReference.java
+++ small-spring-step-15/src/main/java/cn/bugstack/springframework/beans/factory/config/BeanReference.java
@@ -5,6 +5,9 @@ package cn.bugstack.springframework.beans.factory.config;
  * 公众号：bugstack虫洞栈
  * Create by 小傅哥(fustack)
  *
+ * 来自于对开源项目的学习；
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ *
  * Bean 的引用
  */
 public class BeanReference {
