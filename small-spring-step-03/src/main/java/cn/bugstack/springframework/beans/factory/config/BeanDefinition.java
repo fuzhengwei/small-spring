391f7c93b9624fb919da67b8f5267abd63f952cb
小傅哥 <184172133@qq.com>
2022-11-21 16:16:21 +0800

    第 04 章：崭露头角，基于Cglib实现含构造函数的类实例化策略

--- small-spring-step-03/src/main/java/cn/bugstack/springframework/beans/factory/config/BeanDefinition.java
+++ small-spring-step-03/src/main/java/cn/bugstack/springframework/beans/factory/config/BeanDefinition.java
@@ -1,11 +1,6 @@
 package cn.bugstack.springframework.beans.factory.config;
 
 /**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- *
- * 来自于对开源项目的学习；
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public class BeanDefinition {
