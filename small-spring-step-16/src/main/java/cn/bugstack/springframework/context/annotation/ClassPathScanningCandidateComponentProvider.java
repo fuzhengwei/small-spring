d762337acda51f8167c9b2904256b28e5899030f
小傅哥 <184172133@qq.com>
2022-11-21 16:23:44 +0800

    第 17 章：攻无不克，通过三级缓存解决循环依赖

--- small-spring-step-16/src/main/java/cn/bugstack/springframework/context/annotation/ClassPathScanningCandidateComponentProvider.java
+++ small-spring-step-16/src/main/java/cn/bugstack/springframework/context/annotation/ClassPathScanningCandidateComponentProvider.java
@@ -11,11 +11,11 @@ import java.util.Set;
  * A component provider that scans the classpath from a base package. It then
  * applies exclude and include filters to the resulting classes to find candidates.
  * <p>
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
  *
- * 来自于对开源项目的学习；
+ *
+ *
+ *
+ *
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public class ClassPathScanningCandidateComponentProvider {
