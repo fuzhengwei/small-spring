6dcff9d5a242d63933cbeab405c1e0742fda7afc
小傅哥 <184172133@qq.com>
2022-11-21 16:19:12 +0800

    第 09 章：虎行有雨，定义标记类型Aware接口，实现感知容器对象

--- small-spring-step-08/src/main/java/cn/bugstack/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ small-spring-step-08/src/main/java/cn/bugstack/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -21,11 +21,11 @@ import java.lang.reflect.Method;
  * Implements the {@link cn.bugstack.springframework.beans.factory.config.AutowireCapableBeanFactory}
  * interface in addition to AbstractBeanFactory's {@link #createBean} method.
  * <p>
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
  *
- * 来自于对开源项目的学习；
+ *
+ *
+ *
+ *
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory implements AutowireCapableBeanFactory {
