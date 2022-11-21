c7cd0e82bf5fa26e35bdcc98080af21c76f2b591
小傅哥 <184172133@qq.com>
2022-11-21 16:22:03 +0800

    第 14 章：笑傲江湖，通过注解配置和包自动扫描的方式完成Bean对象的注册

--- small-spring-step-13/src/main/java/cn/bugstack/springframework/aop/MethodBeforeAdvice.java
+++ small-spring-step-13/src/main/java/cn/bugstack/springframework/aop/MethodBeforeAdvice.java
@@ -6,11 +6,11 @@ import java.lang.reflect.Method;
  * Advice invoked before a method is invoked. Such advices cannot
  * prevent the method call proceeding, unless they throw a Throwable.
  * <p>
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
  *
- * 来自于对开源项目的学习；
+ *
+ *
+ *
+ *
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public interface MethodBeforeAdvice extends BeforeAdvice {
