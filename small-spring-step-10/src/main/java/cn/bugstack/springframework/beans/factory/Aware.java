0fe19d2c2b89283ac01b611089a591768345a591
小傅哥 <184172133@qq.com>
2022-11-21 16:20:20 +0800

    第 11 章：更上层楼，基于观察者实现，容器事件和事件监听器

--- small-spring-step-10/src/main/java/cn/bugstack/springframework/beans/factory/Aware.java
+++ small-spring-step-10/src/main/java/cn/bugstack/springframework/beans/factory/Aware.java
@@ -10,11 +10,11 @@ package cn.bugstack.springframework.beans.factory;
  *
  * 标记类接口，实现该接口可以被Spring容器感知
  *
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
  *
- * 来自于对开源项目的学习；
+ *
+ *
+ *
+ *
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public interface Aware {
--- small-spring-step-10/src/main/java/cn/bugstack/springframework/context/support/AbstractApplicationContext.java
+++ small-spring-step-10/src/main/java/cn/bugstack/springframework/context/support/AbstractApplicationContext.java
@@ -24,11 +24,11 @@ import java.util.Map;
  * <p>
  * 抽象应用上下文
  * <p>
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
  *
- * 来自于对开源项目的学习；
+ *
+ *
+ *
+ *
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public abstract class AbstractApplicationContext extends DefaultResourceLoader implements ConfigurableApplicationContext {
