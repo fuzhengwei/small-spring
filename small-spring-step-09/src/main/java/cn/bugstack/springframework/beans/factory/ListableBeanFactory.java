2892c4ceb4588d2fa23571870850503771305d5f
小傅哥 <184172133@qq.com>
2022-11-21 16:19:49 +0800

    第 10 章：横刀跃马，关于Bean对象作用域以及FactoryBean的实现和使用

--- small-spring-step-09/src/main/java/cn/bugstack/springframework/beans/factory/ListableBeanFactory.java
+++ small-spring-step-09/src/main/java/cn/bugstack/springframework/beans/factory/ListableBeanFactory.java
@@ -11,11 +11,11 @@ import java.util.Map;
  * preload all their bean definitions (such as XML-based factories) may implement
  * this interface.
  * <p>
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
  *
- * 来自于对开源项目的学习；
+ *
+ *
+ *
+ *
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public interface ListableBeanFactory extends BeanFactory{
