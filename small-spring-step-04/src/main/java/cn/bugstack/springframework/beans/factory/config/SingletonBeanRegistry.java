f4bf8aad94c80bedfd813365b046f0575f37f8fd
小傅哥 <184172133@qq.com>
2022-11-21 15:06:56 +0800

    第 05 章：一鸣惊人，为Bean对象注入属性和依赖Bean的功能实现

--- small-spring-step-04/src/main/java/cn/bugstack/springframework/beans/factory/config/SingletonBeanRegistry.java
+++ small-spring-step-04/src/main/java/cn/bugstack/springframework/beans/factory/config/SingletonBeanRegistry.java
@@ -5,6 +5,9 @@ package cn.bugstack.springframework.beans.factory.config;
  * 公众号：bugstack虫洞栈
  * Create by 小傅哥(fustack)
  *
+ * 来自于对开源项目的学习；
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ *
  * 单例注册表
  */
 public interface SingletonBeanRegistry {
--- small-spring-step-19/src/main/java/cn/bugstack/springframework/aop/framework/ReflectiveMethodInvocation.java
+++ small-spring-step-19/src/main/java/cn/bugstack/springframework/aop/framework/ReflectiveMethodInvocation.java
@@ -13,6 +13,9 @@ import java.lang.reflect.Method;
  * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  * 公众号：bugstack虫洞栈
  * Create by 小傅哥(fustack)
+ *
+ * 来自于对开源项目的学习；
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
  */
 public class ReflectiveMethodInvocation implements MethodInvocation {
 
