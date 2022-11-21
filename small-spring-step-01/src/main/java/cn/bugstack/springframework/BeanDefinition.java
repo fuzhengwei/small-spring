7f1d309f78654580549e73c89fea54270d1e6293
小傅哥 <184172133@qq.com>
2022-11-21 16:15:24 +0800

    第 02 章：小试牛刀，实现一个简单的Bean容器

--- small-spring-step-01/src/main/java/cn/bugstack/springframework/BeanDefinition.java
+++ small-spring-step-01/src/main/java/cn/bugstack/springframework/BeanDefinition.java
@@ -1,15 +1,7 @@
 package cn.bugstack.springframework;
 
 /**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- *
- * 来自于对开源项目的学习；
  * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
-来自于对开源项目的学习；
-https://github.com/DerekYRC/mini-spring
-https://github.com/code4craft/tiny-spring
  */
 public class BeanDefinition {
 
